8c40326fe25898d9c2f7488a0250fa9795e706d3
KIM LEAKHENA <kimleakhena08@gmail.com>
2025-06-08 19:03:19 +0700

    meme

--- Back/index.js
+++ Back/index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
+const productService = require('./services/productService');
 // Connect session
 require('./configs/session')(app);
 
